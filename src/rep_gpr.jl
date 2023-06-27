f26fb2da953502db0da1cd4252dcc0f9975c366e
Daniel Schulman <danieljschulman@gmail.com>
2023-06-27 11:35:03 -0400

    std_gpr and rep_gpr have obs weights

--- src/rep_gpr.jl
+++ src/rep_gpr.jl
@@ -13,10 +13,9 @@ struct RepGPRegression{Tk<:Kernel}
     α
 end
 
-function init_params(::RepGPRegressor, x, y; assume_noise=0.5)
-    n = sum(y.m)
-    u = sum(y.u .* y.m) / n
-    v = sum(y.v .* y.m) / n
+function init_params(::RepGPRegressor, x, y, w; assume_noise=0.5)
+    u = mean(y.u, w)
+    v = mean(y.v, w)
     return (
         mean = u,
         noise_var = positive(v * assume_noise),
@@ -24,18 +23,21 @@ function init_params(::RepGPRegressor, x, y; assume_noise=0.5)
     )
 end
 
-function fit(gpr::RepGPRegressor, x::AbstractMatrix, y::AbstractVector)
+function fit(
+    gpr::RepGPRegressor, x::AbstractMatrix, y::AbstractVector;
+    w=fweights(length.(y))
+)
     ystats = _sufficient_statistics(y)
-    initθ, unflatten = ParameterHandling.value_flatten(init_params(gpr, x, ystats))
+    initθ, unflatten = ParameterHandling.value_flatten(init_params(gpr, x, ystats, w))
     res = Optim.optimize(
-        _zygote_fg!(θ -> _rep_nlml(gpr.kernel, unflatten(θ), x, ystats)),
+        _zygote_fg!(θ -> _rep_nlml(gpr.kernel, unflatten(θ), x, ystats, w)),
         initθ,
         Optim.LBFGS(alphaguess=LineSearches.InitialStatic(scaled=true))
     )
     lml = - minimum(res)
     params = unflatten(Optim.minimizer(res))
     kernel = _kernel(gpr.kernel, params.k)
-    Cchol = _Cchol(kernel, x, ystats, params.noise_var)
+    Cchol = _Cchol(kernel, x, ystats, w, params.noise_var)
     α = Cchol \ (ystats.u .- params.mean)
     return RepGPRegression(lml, params, kernel, Cchol, x, α)
 end
@@ -43,34 +45,34 @@ end
 function _sufficient_statistics(y::AbstractVector)
     u = mean.(y)
 	v = varm.(y, u; corrected=false)
-	return (u=u, v=v, m=length.(y))
+	return (u=u, v=v)
 end
 
-function _rep_nlml(base_kernel::Kernel, params, x, ystats)
+function _rep_nlml(base_kernel::Kernel, params, x, ystats, w)
     kernel = _kernel(base_kernel, params.k)
     n = size(x, 1)
-    Cchol = _Cchol(kernel, x, ystats, params.noise_var)
+    Cchol = _Cchol(kernel, x, ystats, w, params.noise_var)
     z = ystats.u .- params.mean
-	return - _logV(ystats, params.noise_var) + (1/2) * (
+	return - _logV(ystats, w, params.noise_var) + (1/2) * (
 		(n * log2π) +
 		logdet(Cchol) +
 		(z' * (Cchol \ z))
 	)
 end
 
-function _Cchol(kernel::Kernel, x, ystats, noise_var)
-    C = kernelmatrix(kernel, RowVecs(x)) + Diagonal(noise_var ./ ystats.m)
+function _Cchol(kernel::Kernel, x, ystats, w, noise_var)
+    C = kernelmatrix(kernel, RowVecs(x)) + Diagonal(noise_var ./ w)
     return cholesky(C)
 end
 
-function _logV(ystats, noise_var)
-	n = length(ystats.m)
-	sum_m = sum(ystats.m)
-	M = Diagonal(ystats.m)
+function _logV(ystats, w, noise_var)
+	n = length(w)
+	sum_w = sum(w)
+    W = Diagonal(w)
 	return -(1/2) * (
-		((sum_m - n) * (log2π + log(noise_var))) +
-		logdet(M) +
-		((ystats.m ⋅ ystats.v) / noise_var)
+		((sum_w - n) * (log2π + log(noise_var))) +
+		logdet(W) +
+		((w ⋅ ystats.v) / noise_var)
 	)
 end
 
--- src/std_gpr.jl
+++ src/std_gpr.jl
@@ -10,32 +10,35 @@ struct GPRegression
     posterior
 end
 
-function init_params(::GPRegressor, x, y; assume_noise=0.5)
-    yvar = var(y)
+function init_params(::GPRegressor, x, y, w; assume_noise=0.5)
+    yvar = var(y, w)
     return (
-        mean = mean(y),
+        mean = mean(y, w),
         noise_var = positive(yvar * assume_noise),
         k = _init_kernel_params(x; init_var=yvar * (1 - assume_noise))
     )
 end
 
-function fit(gpr::GPRegressor, x::AbstractMatrix, y::AbstractVector)
-    initθ, unflatten = ParameterHandling.value_flatten(init_params(gpr, x, y))
+function fit(
+    gpr::GPRegressor, x::AbstractMatrix, y::AbstractVector;
+    w=uweights(length(y))
+)
+    initθ, unflatten = ParameterHandling.value_flatten(init_params(gpr, x, y, w))
     res = Optim.optimize(
-        _zygote_fg!(θ -> _std_nlml(gpr.kernel, unflatten(θ), x, y)),
+        _zygote_fg!(θ -> _std_nlml(gpr.kernel, unflatten(θ), x, y, w)),
         initθ,
         Optim.LBFGS(alphaguess=LineSearches.InitialStatic(scaled=true))
     )
     lml = - minimum(res)
     params = unflatten(Optim.minimizer(res))
     f = _gp(gpr.kernel, params)
-    post = posterior(f(RowVecs(x), params.noise_var), y)
+    post = posterior(f(RowVecs(x), params.noise_var ./ w), y)
     return GPRegression(lml, params, post)
 end
 
-function _std_nlml(base_kernel::Kernel, params, x, y)
+function _std_nlml(base_kernel::Kernel, params, x, y, w)
     f = _gp(base_kernel, params)
-    return -logpdf(f(RowVecs(x), params.noise_var), y)
+    return -logpdf(f(RowVecs(x), params.noise_var ./ w), y)
 end
 
 function _gp(base_kernel::Kernel, params)
